c2c3e0b3bbdabaea19a7307f10c10f22126ac48e
Vladimir <homie43@yandex.ru>
2023-07-12 22:57:23 +0300

    changed buildDevSrver

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,6 @@ import type { Configuration as DevServerConfiguration } from "webpack-dev-server
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
     port: options.port,
-    open: true, // автоматически в браузере открывает стр с приложением
+    open: true, // автооткрытие страницы с приложением
   };
 }
