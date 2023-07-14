1dac6920c3ed77861f2b0ad55e76c46f3286a8cd
Vladimir <homie43@yandex.ru>
2023-07-14 19:17:34 +0300

    refactoring buildLoaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,23 +1,21 @@
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   // вынес лоадеры в переменную
   // для строгого порядка
 
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
-      // создает `style` из JS строк
-      MiniCssExtractPlugin.loader,
-      // трансллирует CSS в CommonJS
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader, // если разработка, то юзаем "style-loader", если прод то MiniCssExtractPlugin.loader
       "css-loader",
-      // компилирует Sass в CSS
       "sass-loader",
     ],
   };
 
-  // Если используем TS, юзаем этот ладер
+  // Если используем TS, юзаем этот лоадер
   // Если пишем на JS, то добавили бы babel-loader
   const typescriptLoader = {
     test: /\.tsx?$/,
