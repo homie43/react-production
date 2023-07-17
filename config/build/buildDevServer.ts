902a9188fdfa9a4b20e00955eaf682f6295490ca
Vladimir <homie43@yandex.ru>
2023-07-17 15:31:47 +0300

    page proxy bug fix

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,5 +5,6 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
     port: options.port,
     open: true, // автооткрытие страницы с приложением
+    historyApiFallback: true, // проксирует запросы через index.html // для SPA
   };
 }
