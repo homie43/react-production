b5ed3bd1f3a2ca06363ebe10d255f1574cb3eb45
Vladimir <homie43@yandex.ru>
2023-07-19 19:23:44 +0300

    developed a easy func for toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Suspense } from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import Counter from "./components/Counter";
@@ -5,9 +6,20 @@ import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
 const App = () => {
+  const [theme, setTheme] = React.useState<Theme>(Theme.LIGHT);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  };
   return (
-    <div className="app light">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
