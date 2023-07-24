cbd0e1dc5919bfae2c05a8a5b86884c00e0d9c80
Vladimir <homie43@yandex.ru>
2023-07-24 15:32:47 +0300

    the application structure is ready, I start planning the architecture

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./theme/ThemeProvider";
 
+// На данном этапе готова структура приложения
+// Начинаю планировать архитектуру
+// Кажды путь буд подробно комментирвоать
+
 render(
   <BrowserRouter>
     <ThemeProvider>
