280d3d1efcf8c832294e1b34a6470f9377ede687
Vladimir <homie43@yandex.ru>
2023-07-27 18:50:26 +0300

    config on routing completed

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,13 +2,15 @@ import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path={"/about"} element={<AboutPage />} />
-        <Route path={"/"} element={<MainPage />} />
+        {Object.values(routeConfig).map(({ element, path }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
       </Routes>
     </Suspense>
   );
