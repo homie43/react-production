34a01012eb69acec5b4ed4a28eea4f6d6addfe10
Vladimir <homie43@yandex.ru>
2023-08-12 11:57:25 +0300

    theme render by condition

--- src/shared/ui/AppLink/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/AppLink/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "app/providers/ThemeProvider";
+import { Theme, useTheme } from "app/providers/ThemeProvider";
 import classes from "./ThemeSwitcher.module.scss";
 import LightIcon from "shared/assets/icons/theme-light.svg";
 import DarkIcon from "shared/assets/icons/theme-dark.svg";
@@ -12,7 +12,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
   const { theme, toggleTheme } = useTheme();
   return (
     <button className={classNames(classes.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
-      <DarkIcon />
+      {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
     </button>
   );
 };
