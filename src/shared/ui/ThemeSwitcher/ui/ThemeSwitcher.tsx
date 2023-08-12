a4183152113a1339aa572ce593cee9260096cb61
Vladimir <homie43@yandex.ru>
2023-08-12 13:57:46 +0300

    add Button in ThemeSwitcher

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,8 +1,10 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { Theme, useTheme } from "app/providers/ThemeProvider";
-import classes from "./ThemeSwitcher.module.scss";
 import LightIcon from "shared/assets/icons/theme-light.svg";
 import DarkIcon from "shared/assets/icons/theme-dark.svg";
+import { Button } from "shared/ui/Button/Button";
+
+import classes from "./ThemeSwitcher.module.scss";
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -11,8 +13,8 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <button className={classNames(classes.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
+    <Button className={classNames(classes.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
-    </button>
+    </Button>
   );
 };
