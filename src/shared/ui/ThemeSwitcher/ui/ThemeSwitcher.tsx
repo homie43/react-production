1070ada12f6ba1cd5c7042be69944b2b03164250
Vladimir <homie43@yandex.ru>
2023-08-12 13:59:58 +0300

    Nav completed

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,7 +2,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { Theme, useTheme } from "app/providers/ThemeProvider";
 import LightIcon from "shared/assets/icons/theme-light.svg";
 import DarkIcon from "shared/assets/icons/theme-dark.svg";
-import { Button } from "shared/ui/Button/Button";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
 
 import classes from "./ThemeSwitcher.module.scss";
 
@@ -13,7 +13,11 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <Button className={classNames(classes.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
+    <Button
+      theme={ThemeButton.CLEAR} // добавил тему из Button
+      className={classNames(classes.ThemeSwitcher, {}, [className])}
+      onClick={toggleTheme}
+    >
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
     </Button>
   );
