7c1b013f7dccc955a966ce2b905a5a59be012831
Vladimir <homie43@yandex.ru>
2023-08-19 11:54:37 +0300

    change global styles

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -5,11 +5,13 @@ import { routeConfig } from "shared/config/routeConfig/routeConfig";
 const AppRouter = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Routes>
+      <div className="page-wrapper">
+        <Routes>
+          {Object.values(routeConfig).map(({ element, path }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Routes>
+      </div>
     </Suspense>
   );
 };
