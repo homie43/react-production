782b391889cb69e6b6107ec016c8d64431cbcd9c
Vladimir <homie43@yandex.ru>
2023-08-19 11:28:40 +0300

    Sidebar visible

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,16 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { useTheme } from "./providers/ThemeProvider";
+import { Sidebar } from "widgets/Sidebar";
 
 const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
+      <div>
+        <Sidebar />
+      </div>
       <AppRouter />
     </div>
   );
