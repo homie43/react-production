2e65e6c71932dae22360fb384784a9f3dca50fea
Vladimir <homie43@yandex.ru>
2023-08-24 23:24:16 +0300

    set up a simple app translation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,39 @@
-import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { useTheme } from "./providers/ThemeProvider";
 import { Sidebar } from "widgets/Sidebar";
+import { Suspense } from "react";
+import "./styles/index.scss";
+import { useTranslation } from "react-i18next";
+
+const Component = () => {
+  const { t, i18n } = useTranslation();
+
+  const toggle = () => {
+    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
+  };
+
+  return (
+    <div>
+      <button onClick={toggle}>{t("Перевод")}</button>
+      {t("Тестовый пример")}
+    </div>
+  );
+};
 
 const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback="">
+        <Navbar />
+        <Component />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
