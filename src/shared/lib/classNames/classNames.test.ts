7e5cea55c2e3a3939595a58e9b405159910cb668
Vladimir <homie43@yandex.ru>
2023-09-11 23:36:30 +0300

    running two unit tests

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,14 @@
 import { classNames } from './classNames';
 
 describe('classNames', () => {
-    test('test', () => {
+    test('with only first param', () => {
         expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(
+            expected
+        );
     });
 });
