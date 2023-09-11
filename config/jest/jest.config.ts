49734b43981040826a757d5679d04e67da060329
Vladimir <homie43@yandex.ru>
2023-09-11 19:21:53 +0300

    add jest config and command into scripts

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,12 @@
+export default = {
+  clearMocks: true,
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    moduleDirectories: ['node_modules'],
+    testMatch: [
+        // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
+        '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+    ],
+    rootDir: '../../',
+};
