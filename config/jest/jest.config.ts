800f4300e6d25a660cc3b30a2476d3a1391ac9d1
Vladimir <homie43@yandex.ru>
2023-09-11 19:31:57 +0300

    add first test

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,5 +1,5 @@
-export default = {
-  clearMocks: true,
+export default {
+    clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
