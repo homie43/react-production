c363acb553855add5126756f8af524234639310d
Vladimir <homie43@yandex.ru>
2023-10-13 00:14:10 +1000

    fix problem absolute imports for jest

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,7 +3,7 @@ export default {
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
-    moduleDirectories: ['node_modules'],
+    moduleDirectories: ['node_modules', 'src'],
     testMatch: [
         // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
