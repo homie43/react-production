7b07578ca0c03fd10e3a0b55be17bb43f264d994
Vladimir <homie43@yandex.ru>
2023-10-21 23:42:57 +0300

    inst storybook, change wp config, add firs stories

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
+import { buildCssLoaders } from './loaders/buildCssLoaders';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     // вынес лоадеры в переменную
@@ -31,24 +31,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         },
     };
 
-    const cssLoaders = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader, // если разработка, то юзаем "style-loader", если прод то MiniCssExtractPlugin.loader
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]',
-                    },
-                },
-            },
-            'sass-loader',
-        ],
-    };
+    const cssLoaders = buildCssLoaders(isDev);
 
     // Если используем TS, юзаем этот лоадер
     // Если пишем на JS, то добавили бы babel-loader
