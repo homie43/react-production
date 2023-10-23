1aa05ae4504b80da1765d1d72a6e59a8bb7192be
Vladimir <homie43@yandex.ru>
2023-10-23 23:43:47 +0300

    add sidebar stories

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,4 +1,4 @@
-import webpack from 'webpack';
+import webpack, { RuleSetRule } from 'webpack';
 import { BuildPaths } from '../build/types/config';
 import path from 'path';
 import { buildCssLoaders } from '../build/loaders/buildCssLoaders';
@@ -13,6 +13,19 @@ export default ({ config }: { config: webpack.Configuration }) => {
 
     config.resolve.modules.push(paths.src);
     config.resolve.extensions.push('.ts', '.tsx');
+
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+        if (/svg/.test(rule.test as string)) {
+            return { ...rule, exclude: /\.svg$/i };
+        }
+
+        return rule;
+    });
+
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    });
 
     config.module.rules.push(buildCssLoaders(true));
 
