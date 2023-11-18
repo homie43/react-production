03f8291a69b802c39c57ba643c9fabbb909817fd
Vladimir <homie43@yandex.ru>
2023-11-19 01:12:22 +0300

    close Modal on click to Escape

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['react', '@typescript-eslint', 'i18next'],
+    plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks'],
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
@@ -49,6 +49,8 @@ module.exports = {
         'arrow-body-style': 'off',
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
     },
     globals: {
         __IS_DEV__: true,
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -9,20 +9,50 @@ interface ModalProps {
     onClose?: () => void;
 }
 
+const TIMEOUT_DELAY = 300;
+
 export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
-    const closeHandler = () => {
+    const [isClosing, setIsClosing] = React.useState(false);
+    const timeRef = React.useRef<ReturnType<typeof setTimeout>>();
+
+    const closeHandler = React.useCallback(() => {
         if (onClose) {
-            onClose();
+            setIsClosing(true);
+            // плавное закрытие Modal
+            timeRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, TIMEOUT_DELAY);
         }
-    };
+    }, [onClose]);
 
     const mods: Record<string, boolean> = {
         [classes.opened]: isOpen,
+        [classes.isClosing]: isClosing,
     };
 
     const onContentClick = (e: React.MouseEvent) => {
         e.stopPropagation();
     };
+
+    const onKeyDown = React.useCallback(
+        (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                closeHandler();
+            }
+        },
+        [closeHandler]
+    );
+
+    React.useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+        return () => {
+            clearTimeout(timeRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
 
     return (
         <div className={classNames(classes.Modal, mods, [className])}>
