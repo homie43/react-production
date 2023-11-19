464f40bb3fc30f291fac0f0fdfaeae5ccc48f8fc
Vladimir <homie43@yandex.ru>
2023-11-19 23:26:22 +0300

    add comments in Modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,17 +9,17 @@ import { Modal } from 'shared/ui/Modal/Modal';
 
 const App = () => {
     const { theme } = useTheme();
-    const [isOpen, setIsOpen] = React.useState(false);
+    const [isOpen, setIsOpen] = React.useState(false); // отвечает за открытие Modal
     return (
         <div className={classNames('app', {}, [theme])}>
             <React.Suspense fallback=''>
-                <button onClick={() => setIsOpen(true)}>toggle</button>
-
-                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Labore, necessitatibus! Accusantium, vitae. Hic deserunt
+                <button onClick={() => setIsOpen(true)}>Open Modal</button>
+                <Modal isOpen={isOpen} setIsOpen={() => setIsOpen(false)}>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Velit, fuga? Velit sapiente vitae molestiae delectus ad
+                    deserunt soluta culpa, voluptatum error eos nulla beatae id
+                    doloremque fugit fuga optio nemo!
                 </Modal>
-
                 <Navbar />
                 <div className='content-page'>
                     <Sidebar />
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -6,35 +6,40 @@ interface ModalProps {
     className?: string;
     children?: ReactNode;
     isOpen?: boolean;
-    onClose?: () => void;
+    setIsOpen?: () => void;
 }
 
 const TIMEOUT_DELAY = 300;
 
-export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
-    const [isClosing, setIsClosing] = React.useState(false);
+export const Modal = ({
+    className,
+    children,
+    isOpen,
+    setIsOpen,
+}: ModalProps) => {
+    //
+    const [isClosing, setIsClosing] = React.useState(false); // состояние отвечающее за закрытое окно
     const timeRef = React.useRef<ReturnType<typeof setTimeout>>();
 
+    // закрыть Modal
     const closeHandler = React.useCallback(() => {
-        if (onClose) {
+        // useCallback мемоизирует значение функции, чтобы она каждый раз не отрабатывала заново
+        if (setIsOpen) {
             setIsClosing(true);
             // плавное закрытие Modal
             timeRef.current = setTimeout(() => {
-                onClose();
+                setIsOpen();
                 setIsClosing(false);
             }, TIMEOUT_DELAY);
         }
-    }, [onClose]);
+    }, [setIsOpen]);
 
-    const mods: Record<string, boolean> = {
-        [classes.opened]: isOpen,
-        [classes.isClosing]: isClosing,
-    };
-
+    // убираем всплытие событий
     const onContentClick = (e: React.MouseEvent) => {
         e.stopPropagation();
     };
 
+    // логика закрытия модал по нажатия на эскейп
     const onKeyDown = React.useCallback(
         (e: KeyboardEvent) => {
             if (e.key === 'Escape') {
@@ -53,6 +58,11 @@ export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
             window.removeEventListener('keydown', onKeyDown);
         };
     }, [isOpen, onKeyDown]);
+
+    const mods: Record<string, boolean> = {
+        [classes.opened]: isOpen,
+        [classes.isClosing]: isClosing,
+    };
 
     return (
         <div className={classNames(classes.Modal, mods, [className])}>
