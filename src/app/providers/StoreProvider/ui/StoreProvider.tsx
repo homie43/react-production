e6aa741055b8c12b33faf40f75dd308e0ec7e829
Vladimir <homie43@yandex.ru>
2023-11-30 20:49:45 +0300

    add translation

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,16 +2,17 @@ import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createReduxStore } from '../config/sore';
 import { StateShema } from '../config/StateShema';
+import { DeepPartial } from '@reduxjs/toolkit';
 
 interface StoreProviderProps {
     children: ReactNode;
-    initialState?: StateShema;
+    initialState?: DeepPartial<StateShema>;
 }
 
 export const StoreProvider = ({
     children,
     initialState,
 }: StoreProviderProps) => {
-    const store = createReduxStore(initialState);
+    const store = createReduxStore(initialState as StateShema);
     return <Provider store={store}>{children}</Provider>;
 };
--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -4,12 +4,14 @@ import classes from './Counter.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { counterActions } from '../model/slice/counterSlice';
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { useTranslation } from 'react-i18next';
 
 interface CounterProps {
     className?: string;
 }
 
 export const Counter = ({ className }: CounterProps) => {
+    const { t } = useTranslation();
     const counterValue = useSelector(getCounterValue); // в данном случае мы используем реселект
     const dispatch = useDispatch();
     const increment = () => {
@@ -20,9 +22,13 @@ export const Counter = ({ className }: CounterProps) => {
     };
     return (
         <div className={classNames(classes.Counter, {}, [className])}>
-            <h1>{counterValue}</h1>
-            <Button onClick={decrement}>DEC</Button>
-            <Button onClick={increment}>INC</Button>
+            <h1 data-testid='value-title'>{counterValue}</h1>
+            <Button data-testid='decrement-btn' onClick={decrement}>
+                {t('Уменьшение')}
+            </Button>
+            <Button data-testid='increment-btn' onClick={increment}>
+                {t('Увеличение')}
+            </Button>
         </div>
     );
 };
