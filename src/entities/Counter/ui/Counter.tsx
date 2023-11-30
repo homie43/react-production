8f84eaa86941ac7f658e3f88ff5ca46cadfaff98
Vladimir <homie43@yandex.ru>
2023-11-30 20:58:39 +0300

    fix problems

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -6,11 +6,7 @@ import { counterActions } from '../model/slice/counterSlice';
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 import { useTranslation } from 'react-i18next';
 
-interface CounterProps {
-    className?: string;
-}
-
-export const Counter = ({ className }: CounterProps) => {
+export const Counter = () => {
     const { t } = useTranslation();
     const counterValue = useSelector(getCounterValue); // в данном случае мы используем реселект
     const dispatch = useDispatch();
@@ -21,7 +17,7 @@ export const Counter = ({ className }: CounterProps) => {
         dispatch(counterActions.decrement());
     };
     return (
-        <div className={classNames(classes.Counter, {}, [className])}>
+        <div>
             <h1 data-testid='value-title'>{counterValue}</h1>
             <Button data-testid='decrement-btn' onClick={decrement}>
                 {t('Уменьшение')}
